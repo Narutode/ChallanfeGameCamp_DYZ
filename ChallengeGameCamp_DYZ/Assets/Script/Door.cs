5100867c2bed37bcbb3d5ba59d6e4274d2a77e08
Denis ZHENG <dzheng2@myges.fr>
2024-03-14 15:11:32 +0100

    Auto stash before merge of "main" and "origin/main"

--- ChallengeGameCamp_DYZ/Assets/Script/Door.cs
+++ ChallengeGameCamp_DYZ/Assets/Script/Door.cs
@@ -5,4 +5,13 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     public bool canOpen;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+           // gameObject.transform.position.
+        }
+       
+    }
 }
